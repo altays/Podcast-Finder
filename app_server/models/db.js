bffdc6bd6029a22f2d8ea3bea2210b5edfcfbe8c
Alex Taylor <altays633@gmail.com>
2020-10-25 01:05:30 -0400

    adding database connection

---
+++ app_server/models/db.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+const dbURI = 'mongodb://localhost/scavpoddatabase'
+const readLine = require('readline')
+
+mongoose.connect(dbURI, {useUnifiedTopology: true, useNewUrlParser: true})
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${dbURI}`)
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log(`Mongoose connection error:`, err)
+})
+
+mongoose.connection.on('disconnected', (err) => {
+    console.log(`Mongoose disconnected`)
+})
+
+const gracefulShutdown = (msg, callback) => {
+    mongoose.connection.close( () => {
+        console.log(`Mongoose disconnected through ${msg}`);
+        callback();
+    });
+};
+   
+
+if (process.platform === 'win32'){
+    const rl = readLine.createInterface ({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.on ('SIGINT', () => {
+        process.emit ("SIGINT");
+    });
+}
+
+process.once('SIGUSR2', () => {
+        gracefulShutdown('nodemon restart', () => {
+            process.kill(process.pid, 'SIGUSR2');
+    });
+});
+   
+process.on('SIGINT', () => {
+        gracefulShutdown('app termination', () => {
+            process.exit(0);
+    });
+});
+   process.on('SIGTERM', () => {
+        gracefulShutdown('Heroku app shutdown', () => {
+            process.exit(0);
+    });
+});
