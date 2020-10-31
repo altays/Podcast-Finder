9f8eb0c06627e041e69388645f040997a90b139f
Alex Taylor <altays633@gmail.com>
2020-10-31 00:51:02 -0400

    testing heroku deployment server

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,11 +2,9 @@ let searchBar = document.getElementById("text");
 let submitButton = document.getElementById("submit");
 
 const apiContext = {
-    server: 'http://localhost:3000'
- }
- if (process.env.NODE_ENV === 'production'){
-    apiContext.server = 'https://powerful-sierra-93873.herokuapp.com'
- }
+    server: 'https://powerful-sierra-93873.herokuapp.com'
+}
+
 
 redirect = function() {
     if (searchBar.value != "") {
