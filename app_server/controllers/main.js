dcf53b99a686e27690128e7ca0323d3c6d7d2de5
Alex Taylor <altays633@gmail.com>
2020-10-30 23:57:00 -0400

    adding search route

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -10,17 +10,30 @@ const index = async function(req, res) {
    try {
       let apiURL = apiContext.server + "/api/podcasts/"
       axios.get(apiURL).then(response => {
-         res.render('index', {
-            title: 'Scavengers Network Podcast Database',
-            podcasts: response.data
-         })
-         console.log(response.data)
+         renderIndex(req,res,response)
       }) 
    } catch(error){
       res.render('error')
    }
 }
 
+const indexSearch = async function(req, res) {
+   try {
+      let apiURL = apiContext.server + "/api/podcasts/" + req.params.podSearch
+      axios.get(apiURL).then(response => {
+         renderIndex(req,res,response)
+      }) 
+   } catch(error){
+      res.render('error')
+   }
+}
+
+const renderIndex = function(req,res,response){
+   res.render('index', {
+      title: 'Scavengers Network Podcast Database',
+      podcasts: response.data
+   })
+}
 
 const about = (req, res, next) => {
    res.render('about', {title: 'About'})
@@ -28,5 +41,6 @@ const about = (req, res, next) => {
 
 module.exports = {
    index,
-   about
+   about,
+   indexSearch
 }
