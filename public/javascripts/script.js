c8b39a22f34b32dc66bb152be95b3ff890a8f614
Alex Taylor <altays633@gmail.com>
2020-10-30 23:58:27 -0400

    adding logic to pull value from search bar, plug into url

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,19 @@
-const axios = require('axios')
-const apiContext = {
-    server: 'http://localhost:3000'
-}
-if (process.env.NODE_ENV === 'production'){
-    apiContext.server = 'https://powerful-sierra-93873.herokuapp.com/'
-}
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = M.Sidenav.init(elems, {});
+// });
 
+let searchBar = document.getElementById("text");
+let submitButton = document.getElementById("submit");
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
-});
+redirect = function() {
+    // console.log(searchBar.value)
+    if (searchBar.value != "") {
+        let url = "http://www.localhost:3000/search/"+searchBar.value
+        location.href=url
+    } else {
+        location.href = "http://www.localhost:3000/"
+    }
+} 
+
+submitButton.addEventListener('click',redirect)
