a096c6d97d0ff23e4c3cea97b565c082808451fe
Alex Taylor <altays633@gmail.com>
2020-10-31 00:04:07 -0400

    updating url to heroku app

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,18 +1,12 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.sidenav');
-//     var instances = M.Sidenav.init(elems, {});
-// });
-
 let searchBar = document.getElementById("text");
 let submitButton = document.getElementById("submit");
 
 redirect = function() {
-    // console.log(searchBar.value)
     if (searchBar.value != "") {
-        let url = "http://www.localhost:3000/search/"+searchBar.value
+        let url = "https://powerful-sierra-93873.herokuapp.com/search/"+searchBar.value
         location.href=url
     } else {
-        location.href = "http://www.localhost:3000/"
+        location.href = "https://powerful-sierra-93873.herokuapp.com/"
     }
 } 
 
